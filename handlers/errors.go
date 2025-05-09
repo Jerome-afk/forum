c0a473964bf8a0241d78ef78bbc4955b9dd9c1d2
jerootieno <otienojerome7@gmail.com>
2025-05-09 14:45:53 +0300

    Handle: Handling errors

---
+++ handlers/errors.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+// StaticMiddleware handles access to static files
+func StaticMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := "/" + r.URL.Path
+
+		// Block direct access to /static/, /static/css/, /static/js/
+		if path == "/" || strings.HasPrefix(path, "/css/") || strings.HasPrefix(path, "/js/") {
+			w.WriteHeader(http.StatusNotFound)
+			renderTemplate(w, "404.html", nil)
+			return
+		}
+
+		// Otherwise, serve the file
+		next.ServeHTTP(w, r)
+	})
+}
+
+func NoPageHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	renderTemplate(w, "404.html", nil)
+}
+
+func ServerProblemHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	renderTemplate(w, "500.html", nil)
+}
